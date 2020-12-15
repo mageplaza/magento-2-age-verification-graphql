7ba0f925ae141d1a6e0cafa258d8d222357e0aa2
Eric <thainc@mageplaza.com>
2020-12-15 16:37:51 +0700

    update GraphQl Api - done

---
+++ Model/Resolver/Config.php
@@ -0,0 +1,64 @@
+<?php
+/**
+ * Mageplaza
+ *
+ * NOTICE OF LICENSE
+ *
+ * This source file is subject to the Mageplaza.com license that is
+ * available through the world-wide-web at this URL:
+ * https://www.mageplaza.com/LICENSE.txt
+ *
+ * DISCLAIMER
+ *
+ * Do not edit or add to this file if you wish to upgrade this extension to newer
+ * version in the future.
+ *
+ * @category    Mageplaza
+ * @package     Mageplaza_AgeVerificationGraphQl
+ * @copyright   Copyright (c) Mageplaza (https://www.mageplaza.com/)
+ * @license     https://www.mageplaza.com/LICENSE.txt
+ */
+
+declare(strict_types=1);
+
+namespace Mageplaza\AgeVerificationGraphQl\Model\Resolver;
+
+use Magento\Framework\GraphQl\Config\Element\Field;
+use Magento\Framework\GraphQl\Query\ResolverInterface;
+use Magento\Framework\GraphQl\Schema\Type\ResolveInfo;
+use Mageplaza\AgeVerification\Helper\Data;
+
+/**
+ * Class Config
+ * @package Mageplaza\AgeVerificationGraphQl\Model\Resolver
+ */
+class Config implements ResolverInterface
+{
+
+    /**
+     * @var Data
+     */
+    private $_helperData;
+
+    /**
+     * Config constructor.
+     *
+     * @param Data $helperData
+     */
+    public function __construct(
+        Data $helperData
+    ) {
+        $this->_helperData = $helperData;
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function resolve(Field $field, $context, ResolveInfo $info, array $value = null, array $args = null)
+    {
+        return $this->_helperData->getConfigValue(
+            Data::CONFIG_MODULE_PATH,
+            $context->getExtensionAttributes()->getStore()->getId()
+        );
+    }
+}
