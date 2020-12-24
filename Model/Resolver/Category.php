8e6c2ebd4d6340abc0d5bad8fea54883df56bab7
Eric <thainc@mageplaza.com>
2020-12-24 15:08:39 +0700

    Update api - add attribute to Product and Category

---
+++ Model/Resolver/Category.php
@@ -0,0 +1,69 @@
+<?php
+/**
+ * Mageplaza
+ *
+ * NOTICE OF LICENSE
+ *
+ * This source file is subject to the Mageplaza.com license that is
+ * available through the world-wide-web at this URL:
+ * https://www.mageplaza.com/LICENSE.txt
+ *
+ * DISCLAIMER
+ *
+ * Do not edit or add to this file if you wish to upgrade this extension to newer
+ * version in the future.
+ *
+ * @category    Mageplaza
+ * @package     Mageplaza_AgeVerificationGraphQl
+ * @copyright   Copyright (c) Mageplaza (https://www.mageplaza.com/)
+ * @license     https://www.mageplaza.com/LICENSE.txt
+ */
+
+declare(strict_types=1);
+
+namespace Mageplaza\AgeVerificationGraphQl\Model\Resolver;
+
+use Magento\Catalog\Api\Data\ProductInterface;
+use Magento\Catalog\Model\CategoryRepository;
+use Magento\Framework\GraphQl\Config\Element\Field;
+use Magento\Framework\GraphQl\Exception\GraphQlInputException;
+use Magento\Framework\GraphQl\Query\ResolverInterface;
+use Magento\Framework\GraphQl\Schema\Type\ResolveInfo;
+
+/**
+ * Class Category
+ * @package Mageplaza\AgeVerificationGraphQl\Model\Resolver
+ */
+class Category implements ResolverInterface
+{
+    /**
+     * @var CategoryRepository
+     */
+    private $categoryRepository;
+
+    /**
+     * Category constructor.
+     *
+     * @param CategoryRepository $categoryRepository
+     */
+    public function __construct(
+        CategoryRepository $categoryRepository
+    ) {
+        $this->categoryRepository = $categoryRepository;
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function resolve(Field $field, $context, ResolveInfo $info, array $value = null, array $args = null)
+    {
+        if (!isset($value['model'])) {
+            throw new GraphQlInputException(__('"model" value should be specified'));
+        }
+
+        /* @var $product ProductInterface */
+        $product = $this->categoryRepository->get($value['model']->getId());
+
+        return $product->getExtensionAttributes()->getMpAgeVerification();
+    }
+}
